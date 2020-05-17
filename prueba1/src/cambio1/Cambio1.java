f59aeedd69bd40c6e733d30e31e09b39b713ed76
Usu2 <Usu2@example.com>
2020-05-17 21:11:21 +0200

    Añado una línea a la clase Cambio1

--- prueba1/src/cambio1/Cambio1.java
+++ prueba1/src/cambio1/Cambio1.java
@@ -2,6 +2,7 @@ package cambio1;
 public class Cambio1 {
 	public static void main(String[] args) {
 		 System.out.println("Pruebas Git. Clase Cambio1");
+		 System.out.println("Pruebas Git. Clase Cambio1");
 	}
 }
 
