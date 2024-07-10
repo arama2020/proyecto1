fc69f4978de3e836b28c1c94ffbb3dfa17dc3191
Usu2 Name <usu2@example.com>
2024-07-10 17:32:54 +0200

    Segundo cambio en Principal. Momento 3.

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -5,5 +5,6 @@ public class Principal {
 	public static void main(String[] args) {
 	 	 System.out.println("Hola mundo java-git");
 		 System.out.println("Realizo el primer cambio. Añado una visualización.");
+		 System.out.println("Realizo el segundo cambio. Añado una visualización.");
 	}
 }
