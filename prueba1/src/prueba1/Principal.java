26551cdbb51906618094c854ebcde53519beb30a
Usu2 Name <usu2@example.com>
2024-07-10 17:32:27 +0200

    Primer cambio en la clase Principal. Momento 2.

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -4,6 +4,6 @@ public class Principal {
 
 	public static void main(String[] args) {
 	 	 System.out.println("Hola mundo java-git");
-
+		 System.out.println("Realizo el primer cambio. Añado una visualización.");
 	}
 }
